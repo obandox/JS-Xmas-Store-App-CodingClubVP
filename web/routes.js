dc6c0cf36514cb592d4db34742a86275c7bd5550
Miguel Obando <miguel@obando.com.ve>
2015-12-09 19:35:49 -0500

    added cors

--- web/routes.js
+++ web/routes.js
@@ -9,11 +9,29 @@ var resources = require('../config.json').resources;
 var inspector = require('../helpers/inspector');
 var middleware = require('../helpers/middleware');
 
+
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Location, X-Timezone');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // intercept OPTIONS method
+    if (req.method == 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+};
+
+
 // settings
 var views = inspector.addPluginsWeb(web);
 views.push(__dirname.replace('/web', '/views'));
 web.set('views', views);
 web.set('view engine', 'jade');
+web.use(allowCrossDomain);
 web.use(express.static(__dirname.replace('/web', '/public')));
 // web.use(middleware.trusted);
 web.use(middleware.robots);
